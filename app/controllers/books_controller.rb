622492b45641caca27b47a3145a058058d9a9b1e
EC2 Default User <ec2-user@ip-172-31-42-14.ec2.internal>
2021-03-31 13:22:09 +0000

    [Finish]BookersRE

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,51 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        redirect_to book_path(@book)
+        flash[:notice] = "Book was successfully updated."
+    else
+        render :edit
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully created."
+       # redirect_to books_path
+       # 投稿後、遷移先をbooksのindexページにするなら
+    else
+      @books = Book.all
+      render :index
+    end
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
